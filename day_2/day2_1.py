17010310bddbc5eb68d9c659765c94c37053059e
Gustav Andersson <gustav.k.andersson@gmail.com>
2018-12-05 13:27:22 +0100

    Rewrote day2_1 using defaultdict

--- day_2/day2_1.py
+++ day_2/day2_1.py
@@ -1,31 +1,20 @@
+from collections import defaultdict
 from day2_data import DATA as DATA
 
-def find_pair_triplet(word):
-    pair = False
-    triplet = False
-    counts = {}
-    for c in word:
-        if c in counts:
-            counts[c] = counts[c] + 1;
-        else:
-            counts[c] = 1
-        
-    for c in counts:
-        if counts[c] == 2:
-            pair = True
-        elif counts[c] == 3:
-            triplet = True
+def count_chars(string):
+    char_count = defaultdict(int)
+    for c in string:
+        char_count[c] += 1
     
-    return pair, triplet
-
+    return char_count
 
 def main(data):
     pairs = 0
     triplets = 0
     for i in data:
-        pair, triplet = find_pair_triplet(i);
-        pairs += pair
-        triplets += triplet
+        c = count_chars(i);
+        pairs += 2 in c.values()
+        triplets += 3 in c.values()
 
     print str(pairs) + " with pairs " +str(triplets) + " with triplets, checksum: " +str(pairs * triplets) 
 
