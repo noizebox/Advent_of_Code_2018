26d839128a41677be3c7b56a2d6aca35a100d410
Gustav Andersson <gustav.k.andersson@gmail.com>
2018-12-05 15:15:46 +0100

    Rewrote day2_1 using Counter

--- day_2/day2_1.py
+++ day_2/day2_1.py
@@ -1,18 +1,11 @@
-from collections import defaultdict
+from collections import Counter
 from day2_data import DATA as DATA
-
-def count_chars(string):
-    char_count = defaultdict(int)
-    for c in string:
-        char_count[c] += 1
-    
-    return char_count
 
 def main(data):
     pairs = 0
     triplets = 0
     for i in data:
-        c = count_chars(i);
+        c = Counter(i);
         pairs += 2 in c.values()
         triplets += 3 in c.values()
 
